3ac13c03f14b0ac44140faf72d6ff85727c0df84
Viliam Holub <vilda@logentries.com>
2015-01-22 12:13:57 +0100

    LOG-3008 Adding initial implementation for plan and syslog formatters

--- src/formatters.py
+++ src/formatters.py
@@ -1,6 +1,6 @@
 
 # coding: utf-8
-# ex: set tabstop=4 shiftwidth=4 expandtab
+# vim: set ts=4 sw=4 et:
 
 __author__ = 'Logentries'
 
@@ -12,26 +12,35 @@ import socket
 
 
 class FormatPlain(object):
+
     """Formats lines as plain text, prepends each line with token."""
 
     def __init__(self, token):
-        self.token = token
+        self._token = token
 
     def format_line(self, line):
-        return self.token + line
+        return self._token + line
+
 
 class FormatSyslog(object):
+
     """Formats lines according to Syslog format RFC 5424. Hostname is taken
     from configuration or current hostname is used."""
 
-    def __init__(self, hostname, appname):
+    def __init__(self, hostname, appname, token):
         if hostname:
             self._hostname = hostname
         else:
             self._hostname = socket.gethostname()
         self._appname = appname
+        self._token = token
 
     def format_line(self, line):
-        return '<14>1 %sZ %s %s - - - %s' % (datetime.datetime.utcnow().isoformat('T'),
-                self._hostname, self._appname, line)
+        return '%s<14>1 %sZ hotname=%s appname=%s - - - %s' % (self._token,
+                datetime.datetime.utcnow().isoformat('T'), self._hostname,
+                self._appname, line)
 
+    def format_line_ext(self, msgid, line):
+        return '%s<14>1 %sZ hostname=%s appname=%s - msgid=%s - %s' % (self._token,
+                datetime.datetime.utcnow().isoformat('T'), self._hostname,
+                self._appname, msgid, line)
