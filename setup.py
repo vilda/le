3ed353dca7452adc27bdd29454257348228593fd
Jenkins <jenkins@logentries.com>
2015-02-19 11:48:30 +0000

    Release new version 1.4.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 
 setup(name='logentries',
-      version="1.4.7",
+      version="1.4.8",
       description='Logentries Linux agent',
       author='Logentries',
       author_email='hello@logentries.com',
