4a10536226ba58ab9b1fcc505d08039ee3038c7f
Mr Jenkins <jenkins@logentries.com>
2015-05-06 13:31:49 +0000

    Release new version 1.4.15

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 
 setup(name='logentries',
-      version="1.4.14",
+      version="1.4.15",
       description='Logentries Linux agent',
       author='Logentries',
       author_email='hello@logentries.com',
