1ab550959cc7f6b6d756106a898e6b11ae684991
Mr Jenkins <jenkins@logentries.com>
2015-12-01 15:09:48 +0000

    Release new version 1.4.26

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 
 setup(name='logentries',
-      version="1.4.25",
+      version="1.4.26",
       description='Logentries Linux agent',
       author='Logentries',
       author_email='hello@logentries.com',
