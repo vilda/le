12e584effa2387de142bbfc092c88efab90a4fc0
Mr Jenkins <jenkins@logentries.com>
2016-01-05 13:58:41 +0000

    Release new version 1.4.28

--- test/tests.d/help.sh
+++ test/tests.d/help.sh
@@ -9,7 +9,7 @@ Scenario 'Help'
 Testcase 'List help usage'
 
 $LE
-#e Logentries agent version 1.4.27
+#e Logentries agent version 1.4.28
 #e usage: le COMMAND [ARGS]
 #e 
 #e Where command is one of:
